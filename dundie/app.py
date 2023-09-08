3b49f9b9fba12ff91ed9bcd770efaa82380a8e93
Alex Furtunato <alex.furtunato@lais.huol.ufrn.br>
2023-09-08 18:07:54 -0300

    injecao de dependencias

--- dundie/app.py
+++ dundie/app.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from dundie.models.user import User, UserResponse
+from sqlmodel import Session, select
+from dundie.db import ActiveSession
 
 app = FastAPI(
     title="Dundie",
--- dundie/db.py
+++ dundie/db.py
@@ -1,8 +1,15 @@
-from sqlmodel import create_engine
+from sqlmodel import Session, create_engine
 from .config import settings
+from fastapi import Depends
 
 engine = create_engine(
     url=settings.db.uri,
     echo=settings.db.echo,
     connect_args=settings.db.connect_args,
 )
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+ActiveSession = Depends(get_session)
