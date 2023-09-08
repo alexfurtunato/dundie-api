06b94d7a52b6705f1b744b1d9be104711b9cb77f
Alex Furtunato <alex.furtunato@lais.huol.ufrn.br>
2023-09-08 17:40:06 -0300

    - segurança - create-user

--- dundie/models/user.py
+++ dundie/models/user.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from sqlmodel import SQLModel, Field
+from dundie.security import HashedPassword
 
 
 class User(SQLModel, table=True):
@@ -8,7 +9,7 @@ class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(nullable=False)
     email: str = Field(nullable=None)
-    password: str = Field(nullable=None)
+    password: HashedPassword
     name: str = Field(nullable=None)
     avatar: Optional[str] = None
     bio: Optional[str] = None
@@ -20,3 +21,6 @@ class User(SQLModel, table=True):
         """Users belonging to management dept are admins"""
         return self.dept == "management"
 
+def generate_username(name: str) -> str:
+    """Generates a slug username from a name"""
+    return name.lower().replace(" ", "-")
