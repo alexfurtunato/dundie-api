b8704beb0f6ea6ea9e5072fa53f92fa7dc7599b6
Alex Furtunato <alex.furtunato@lais.huol.ufrn.br>
2023-09-08 17:56:21 -0300

    User Serializers

--- dundie/models/user.py
+++ dundie/models/user.py
@@ -1,3 +1,4 @@
+from pydantic import BaseModel, root_validator
 from typing import Optional
 from sqlmodel import SQLModel, Field
 from dundie.security import HashedPassword
@@ -21,6 +22,38 @@ class User(SQLModel, table=True):
         """Users belonging to management dept are admins"""
         return self.dept == "management"
 
+
 def generate_username(name: str) -> str:
     """Generates a slug username from a name"""
     return name.lower().replace(" ", "-")
+
+
+class UserResponse(BaseModel):
+    """Serializer for User Response"""
+
+    name: str
+    username: str
+    dept: str
+    avatar: Optional[str] = None
+    bio: Optional[str] = None
+    currency: str
+
+
+class UserRequest(BaseModel):
+    """Serializer for User request payload"""
+
+    name: str
+    email: str
+    dept: str
+    password: str
+    currency: str = "USD"
+    username: Optional[str] = None
+    avatar: Optional[str] = None
+    bio: Optional[str] = None
+
+    @root_validator(pre=True)
+    def generate_username_if_not_set(cls, values):
+        """Generates username if not set"""
+        if values.get("username") is None:
+            values["username"] = generate_username(values["name"])
+        return values
